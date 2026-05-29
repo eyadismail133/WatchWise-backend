57bd0785e62d287de704296a63854e4e795e2596
eyad <eyadismail764@gmail.com>
2026-05-29 17:03:42 +0300

    fixing more and more

--- lib/auth.ts
+++ lib/auth.ts
@@ -31,9 +31,6 @@ export const auth = betterAuth({
     database: {
       generateId: "serial",
     },
-    crossSubDomainCookies: {
-      enabled: true,
-    },
     cookies: {
       session_token: {
         name: "auth_session",
