997071ca58319fcddca5db1b61bf6ab712da9155
eyad <eyadismail764@gmail.com>
2026-05-29 14:51:51 +0300

    fix safari issue

--- lib/auth.ts
+++ lib/auth.ts
@@ -36,7 +36,7 @@ export const auth = betterAuth({
         name: "auth_session",
         attributes: {
           httpOnly: true,
-          secure: true,
+          secure: isProduction,
           sameSite: isProduction ? "none" : "lax",
           path: "/",
         },
